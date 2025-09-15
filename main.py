319680ba1c44d0b006a3aac67de2aed454ac2580
cindy_hsiao <wenwenstudio218@gmail.com>
2025-09-15 16:46:56 +0800

    6ver

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from flask import Flask
 
 app = Flask(__name__)
-@app.route('/')
+@app.route('/') #根目錄
 def index():
     return '<h1 style="color:red">您好，Flask！</h1>'
+
+@app.route("/name") #路由節點
+def name():
+    return '<h1>您好，Flask！</h1>'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
